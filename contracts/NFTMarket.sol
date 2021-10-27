696c87f9f352b2dfb8991c97b4a865b1b69cc6ae
Merdi-kim <merdikimuha1@gmail.com>
2021-10-27 06:01:51 -0700

    set smartcontracts

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,148 @@
+//SPDX-License-Identifier:UNLICENSED
+pragma solidity>0.8.0;
+
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+    uint listingPrice = 0.025 ether;
+
+    constructor() {
+       owner = payable(msg.sender); 
+    }
+
+    struct MarketItem {
+        uint itemId;
+        address nftContract;
+        uint tokenId;
+        address payable seller;
+        address payable owner;
+        uint price;
+        bool sold;
+    }
+
+    mapping (uint256 => MarketItem) private idToMarketItem;
+
+    event MarketItemCreated (
+        uint indexed itemId,
+        address indexed nftContract,
+        uint indexed tokenId,
+        address payable seller,
+        address payable owner,
+        uint price,
+        bool sold
+    );
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function createMarketItem (
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        require(price > 0, "Price must be at least 1 wei");
+        require(msg.value == listingPrice, "Price must be equal to listing Price");
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        emit MarketItemCreated(itemId, nftContract, tokenId, payable(msg.sender), payable(address(0)), price, false);
+    }
+
+    function createMarketSale(
+        address nftContract, 
+        uint itemId
+    ) public payable nonReentrant {
+        uint price = idToMarketItem[itemId].price;
+        uint tokenId = idToMarketItem[itemId].tokenId;
+        require(msg.value == price, "Please send the asked price");
+        idToMarketItem[itemId].seller.transfer(msg.value);
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        idToMarketItem[itemId].sold = true;
+        _itemsSold.increment();
+        payable(owner).transfer(listingPrice);
+    }
+
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint itemCount = _itemIds.current();
+        uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
+        uint currentIndex = 0;
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+        for (uint i = 0; i < itemCount; i++) {
+            if (idToMarketItem[i + 1].owner == address(0)) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if(idToMarketItem[i+1].owner == msg.sender) {
+                itemCount += 1;
+            }
+            
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i +1].owner == msg.sender) {
+                uint currentId = idToMarketItem[i+1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+    }
+
+    function fetchItemsCreated() public view returns (MarketItem[] memory) {
+        uint totalitemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i=0; i<totalitemCount; i++) {
+            if (idToMarketItem[i+1].seller == msg.sender) {
+                itemCount += 1;
+            }
+        }
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint i = 0; i<totalitemCount; i++) {
+            if (idToMarketItem[i+1].seller == msg.sender) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle");
+
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+//https://polygon-mumbai.infura.io/v3/871b810ff04e4f4aa63c36e774350a12
+module.exports = {
+  networks: {
+    development: {
+      chainId:1337
+    },
+    mumbai: {
+      url: 'https://polygon-mumbai.infura.io/v3/871b810ff04e4f4aa63c36e774350a12',
+      accounts: [process.env.METAMASK_PRIVATE_KEY]
+    }, 
+    mainnet: {
+      url: 'https://polygon-mainnet.infura.io/v3/871b810ff04e4f4aa63c36e774350a12',
+      accounts: []
+    }
+  },
+  solidity: "0.8.4",
+};
+
