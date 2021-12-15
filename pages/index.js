a485613923cb26c7e7a1d06d6725198b502c222e
Merdi-kim <merdikimuha1@gmail.com>
2021-12-14 23:27:27 -0800

    add passHref

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         <div className='flex-1 h-screen flex flex-col items-center justify-center'>
           <section className='text-4xl flex-reverse sm:text-6xl font-extrabold mb-12 pl-12 w-full font-tt'>Discover and sell awesome <span className='text-purple-600'>NFTs</span></section>
           <p className='w-10/12 mb-16 font-merkim'>Buy,sell and own digital art from your favorite artists and enjoy low gas fees on polygon. All you need is a wallet with $MATIC and you're all set. Now launch the app and enjoy digital arts</p>
-          <section className='flex w-full justify-start px-12 mb-16'><Link href={'/home'}><span className='bg-purple-600 text-2xl py-3 px-6 rounded-xl border-2 border-white cursor-pointer font-tt text-white'>Explore now </span></Link></section>
+          <section className='flex w-full justify-start px-12 mb-16'><Link href={'/home'} passHref><span className='bg-purple-600 text-2xl py-3 px-6 rounded-xl border-2 border-white cursor-pointer font-tt text-white'>Explore now </span></Link></section>
           <section className='flex w-full justify-start pl-12 font-merkim'>
             <CountCard number={30} name={'Artists'}/>
             <CountCard number={100} name={'NFTs'}/>
